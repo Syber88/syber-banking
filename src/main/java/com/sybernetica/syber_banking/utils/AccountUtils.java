8f3bf70aeee36e11dd08eddf8f9fbad8e035b398
Siyabonga Magwaza <smagwaza023@student.wethinkcode.co.za>
2025-01-03 02:04:08 +0200

    partially done with utility function that'll create account numbers

---
+++ src/main/java/com/sybernetica/syber_banking/utils/AccountUtils.java
@@ -0,0 +1,19 @@
+package com.sybernetica.syber_banking.utils;
+
+import java.time.Year;
+
+public class AccountUtils {
+    /**
+     * the accoount number should have the branch code along with the
+     * current year of creation of the account
+     * then random numbers
+     */
+    private String branchCode = "001";
+    private String currentYear = String.valueOf(Year.now());
+    
+    publiuc String year())
+
+    
+
+
+}
