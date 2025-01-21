54ad4872cb55856f4aefd84c95eec01d1a174671
Siyabonga Magwaza <smagwaza023@student.wethinkcode.co.za>
2025-01-21 17:04:06 +0200

    done with email service implementation

--- src/main/java/com/sybernetica/syber_banking/service/impl/EmailServiceImpl.java
+++ src/main/java/com/sybernetica/syber_banking/service/impl/EmailServiceImpl.java
@@ -3,6 +3,8 @@ package com.sybernetica.syber_banking.service.impl;
 import com.sybernetica.syber_banking.dto.EmailDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 import org.springframework.mail.javamail.JavaMailSender;
 
@@ -17,6 +19,18 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendEmailAlert(EmailDetails emailDetails) {
+        try {
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(senderEmail);
+            mailMessage.setTo(emailDetails.getRecipient());
+            mailMessage.setText(emailDetails.getMessageBody());
+            mailMessage.setSubject(emailDetails.getSubject());
+
+            javaMailSender.send(mailMessage);
+            System.out.println("Mail sent successfully");
+        } catch (MailException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
