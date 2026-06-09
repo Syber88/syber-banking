1c844444981c0c0975972b0bc4b743bb314abc5a
Siyabonga Magwaza <148061326+Syber88@users.noreply.github.com>
2026-06-09 14:40:16 +0200

    Delete src directory

--- src/main/java/com/sybernetica/syber_banking/service/impl/EmailService.java
+++
@@ -1,8 +0,0 @@
-package com.sybernetica.syber_banking.service.impl;
-
-import com.sybernetica.syber_banking.dto.EmailDetails;
-
-public interface EmailService {
-
-    void sendEmailAlert(EmailDetails emailDetails);
-}
