fb3a542fa35742b8f4c0b53286c9ee290594a31c
Siyabonga Magwaza <ssmagwaza88@gmail.com>
2026-06-15 12:10:20 +0200

    linked customer and account

--- src/main/java/com/syber/banking/entitiy/Account.java
+++ src/main/java/com/syber/banking/entitiy/Account.java
@@ -1,9 +1,6 @@
 package com.syber.banking.entitiy;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import java.math.BigDecimal;
 
@@ -14,7 +11,10 @@ public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String customerId; /*Foreign key*/
+
+    @ManyToOne
+    @JoinColumn(name="customer_id")
+    private Customer customer; /*Foreign key*/
     private Long accountNumber;
     private BigDecimal balance;
     private String accountTye;
--- src/main/java/com/syber/banking/entitiy/Customer.java
+++ src/main/java/com/syber/banking/entitiy/Customer.java
@@ -1,13 +1,12 @@
 package com.syber.banking.entitiy;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -19,6 +18,9 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @OneToMany(mappedBy="customer")
+    private List<Account> accounts;
     private String firstName;
     private String lastName;
     private String email;
