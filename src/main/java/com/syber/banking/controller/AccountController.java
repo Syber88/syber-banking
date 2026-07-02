050ec93076817b95d01790213bdf811ba88b309b
Siyabonga Magwaza <ssmagwaza88@gmail.com>
2026-07-02 21:23:38 +0200

    refactor: added swagger documentation

--- src/main/java/com/syber/banking/controller/AccountController.java
+++ src/main/java/com/syber/banking/controller/AccountController.java
@@ -6,10 +6,13 @@ import com.syber.banking.dto.response.AccountResponse;
 import com.syber.banking.dto.response.DepositResponse;
 import com.syber.banking.entity.Account;
 import com.syber.banking.service.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/accounts")
+@Tag(name = "Accounts", description = "Operations for managing customer bank accounts")
 public class AccountController {
 
     private final AccountService accountService;
@@ -18,16 +21,28 @@ public class AccountController {
         this.accountService = accountService;
     }
 
+    @Operation(
+            summary = "Create a bank account",
+            description = "Creates a new bank account for an existing customer."
+    )
     @PostMapping("")
     public AccountResponse createAccount(@RequestBody CreateAccountRequest request) {
          return accountService.createAccount(request.getCustomerId(),request.getAccountType());
     }
 
+    @Operation(
+            summary = "Deposit money",
+            description = "Deposits funds into an existing bank account."
+    )
     @PostMapping("/{accountId}/deposit")
     public DepositResponse deposit(@PathVariable Long accountId, @RequestBody DepositRequest request){
         return  accountService.deposit(accountId, request.getAmount());
     }
 
+    @Operation(
+            summary = "Retrieve account",
+            description = "Returns account details using the account ID."
+    )
     @GetMapping("/{accountId}")
     public AccountResponse getAccountById(@PathVariable Long accountId) {
         Account account = accountService.getAccountById(accountId);
